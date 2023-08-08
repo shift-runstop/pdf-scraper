33ae67ea3124df320734214641a36a78ec802833
shift-runstop <shift-runstop@protonmail.ch>
2023-08-08 22:07:48 +0100

    switched to PyMuPDF, extracts the text from each PDF, and saves the text into separate text files::WORKING

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-import subprocess
+import os
+import fitz
 
-from pathlib import Path
-from pdfminer.high_level import extract_text
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    pdf_document = fitz.open(pdf_path)
 
-pdf_search = Path("./files/").glob("*.pdf")
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        text += page.get_text("text")
 
-pdf_files = pdf_files = [str(file.absolute()) for file in pdf_search]
+    pdf_document.close()
+    return text
 
+def main():
+    pdf_folder = "./files"
+    output_folder = "./output"
 
-for pdf in pdf_files:
-        text = extract_text("*.pdf")
-        print(text, file=output.txt)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for pdf_filename in os.listdir(pdf_folder):
+        if pdf_filename.endswith(".pdf"):
+            pdf_path = os.path.join(pdf_folder, pdf_filename)
+            text = extract_text_from_pdf(pdf_path)
+
+            text_filename = os.path.splitext(pdf_filename)[0] + ".txt"
+            text_path = os.path.join(output_folder, text_filename)
+
+            with open(text_path, "w", encoding="utf-8") as text_file:
+                text_file.write(text)
+
+            print(f"Extracted text from {pdf_filename} and saved as {text_filename}")
+
+if __name__ == "__main__":
+    main()
